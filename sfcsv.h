69b1f005f24b55b9d5d703d82f8c707ae605aff2
labyrinthofdreams <noemail@localhost.localhost>
2014-06-12 17:06:24 +0300

    initial

---
+++ sfcsv.h
@@ -0,0 +1,152 @@
+#ifndef SFCSV_H
+#define SFCSV_H
+
+#include <algorithm>
+#include <iterator>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+#include <vector>
+
+namespace sfcsv {
+
+template <class OutIter>
+void parse_line(const std::string&, OutIter, const char);
+
+std::vector<std::string> parse_line(const std::string &s, const char sep = ',') {
+    std::vector<std::string> out;
+
+    parse_line(s, std::back_inserter(out), sep);
+
+    return out;
+}
+
+/**
+ * @brief Parse a CSV line from std::string
+ * @pre Parameter out must satisfy OutputIterator
+ * @param out Output iterator
+ * @param sep Field separator
+ * @throws std::runtime_error
+ */
+template <class OutIter>
+void parse_line(const std::string& s, OutIter out, const char sep = ',') {
+    if(s.empty()) {
+        *out++ = s;
+        return;
+    }
+
+    bool in_quotes = false;
+    int num_quotes = 0;
+    std::string field = "";
+
+    for(auto it = s.begin(), end = s.end(); it != end; ++it) {
+        const char c = *it;
+        if(c == '"') {
+            /**** Not sure if this is ever even possible?
+            if(!in_quotes && !field.empty()) {
+                throw std::runtime_error("Double quotes not permitted outside fields");
+            }****/
+            ++num_quotes;
+
+            const bool last = ((it + 1) == end); // end of string?
+            if(!(last || *(it + 1) != '"')) {
+                continue;
+            }
+            if((num_quotes % 2) == 0) { // even number of quotes
+                if(field.empty()) { // only quotes in this field
+                    if(num_quotes == 2 && c == sep) { // empty field
+                        *out++ = "";
+                    }
+                    else {
+                        field.append(((num_quotes - 2) / 2), '"');
+                    }
+                }
+                else {
+                    field.append((num_quotes / 2), '"');
+                }
+            }
+            else {
+                // Odd number of quotes, a field should either start or end
+                in_quotes = !in_quotes;
+                if(num_quotes > 1) {
+                    field.append(((num_quotes - 1) / 2), '"');
+                }
+            }
+            num_quotes = 0;
+            if(!in_quotes && !last && *(it + 1) != sep) {
+                // If next character after double quote is not a separator
+                throw std::runtime_error("Invalid character / unknown separator after a field: " + *(it + 1));
+            }
+        }
+        else if(c == sep && !in_quotes) {
+            // Separator ends field
+            *out++ = field;
+            field.erase();
+        }
+        else if(c == '\n' && !in_quotes) {
+            // Newline characters are not permitted
+            // outside of double-quoted fields
+            throw std::runtime_error("Invalid newline character not within a field");
+        }
+        else if(c == ' ' && !in_quotes) {
+            throw std::runtime_error("Spaces are not permitted in non-quoted fields");
+        }
+        else {
+            field += c;
+        }
+    }
+
+    // Push last field in result
+    *out++ = field;
+}
+
+/**
+ * @brief Encode a single string field
+ *
+ * This function duplicates all quote characters
+ * and encloses the string with quote characters
+ *
+ * @pre Parameter s must have .reserve(size), operator+=,
+ * and it must have .begin()/.end() that satisfy InputIterator
+ *
+ * @param s String to encode
+ * @return Encoded string
+ */
+template <class StringT>
+StringT encode_field(const StringT& s) {
+    StringT out;
+    // Reserve space for old string, surrounding quotes, duplicated quotes
+    out.reserve(s.size() + 2 + std::count(std::begin(s), std::end(s), '"'));
+    out += '"';
+    for(const auto c : s) {
+        out += c;
+        if(c == '"') {
+            out += c;
+        }
+    }
+    out += '"';
+    return out;
+}
+
+/**
+ * @brief Encode strings from iterator range start to end
+ * @pre Start and end parameters must satisfy InputIterator
+ * @pre Out parameter must satisfy OutputIterator.
+ * @param start Iterator to the begin position
+ * @param end Iterator to the end position
+ * @param out Iterator to output
+ * @param sep Field separator
+ */
+template <class InIter, class OutIter, class Char = char>
+void encode_line(InIter start, InIter end, OutIter out, const Char* sep = ",") {
+    while(start != end) {
+        *out++ = encode_field(*start);
+        if(++start != end) {
+            *out++ = sep;
+        }
+    }
+}
+
+} // namespace sfcsv
+
+#endif // SFCSV_H
---
+++ tests/main.cpp
@@ -0,0 +1,230 @@
+#include <iostream>
+#include <iterator>
+#include <vector>
+#include <string>
+#include "sfcsv.h"
+#include "gtest/gtest.h"
+
+class ParserTest : public ::testing::Test {
+protected:
+    std::vector<std::string> all;
+    std::vector<std::string> result;
+
+    template <class ...Args>
+    bool vec_eq(Args ...args) {
+        return result == std::vector<std::string>({args...});
+    }
+
+    void parse(const std::string& s) {
+        result = sfcsv::parse_line(s);
+        for(const auto& r : result) {
+            all.push_back(r);
+        }
+    }
+
+    void print() {
+        std::cout << "Elements: " << std::endl;
+        for(const auto& r : result) {
+            std::cout << r << std::endl;
+        }
+        std::cout << std::endl;
+    }
+
+    void print_all() {
+        std::cout << "All elements: " << std::endl;
+        for(const auto& r : all) {
+            std::cout << r << std::endl;
+        }
+        std::cout << std::endl;
+    }
+};
+
+TEST_F(ParserTest, NonQuotedFields)
+{	
+    parse("hello");
+    EXPECT_TRUE(vec_eq("hello"));
+
+    parse("hello,world");
+    EXPECT_TRUE(vec_eq("hello", "world"));
+
+    parse("hello,world,100.0");
+    EXPECT_TRUE(vec_eq("hello", "world", "100.0"));
+}
+
+TEST_F(ParserTest, QuotedFields)
+{
+    parse(R"("hello")");
+    EXPECT_TRUE(vec_eq("hello"));
+
+    parse(R"("hello","world")");
+    EXPECT_TRUE(vec_eq("hello", "world"));
+
+    parse(R"("hello","world","foobar")");
+    EXPECT_TRUE(vec_eq("hello", "world", "foobar"));
+}
+
+/**
+ * By default, spaces are not allowed in non-quoted fields
+ * nor in quoted fields between the separator
+ */
+TEST_F(ParserTest, NonQuotedFieldsWithSpaces)
+{
+    ASSERT_ANY_THROW(parse("hello world"));
+
+    ASSERT_ANY_THROW(parse("hello world,bye world"));
+
+    ASSERT_ANY_THROW(parse("hello, world"));
+
+    ASSERT_ANY_THROW(parse("hello ,world"));
+
+    ASSERT_ANY_THROW(parse("hello , world"));
+}
+
+TEST_F(ParserTest, QuotedFieldsWithSpaces)
+{
+    ASSERT_ANY_THROW(parse(R"("hello", "world")"));
+
+    ASSERT_ANY_THROW(parse(R"("hello" ,"world")"));
+
+    ASSERT_ANY_THROW(parse(R"("hello" , "world")"));
+}
+
+TEST_F(ParserTest, MixedFields)
+{
+    parse(R"(hello,"world",foo)");
+    EXPECT_TRUE(vec_eq("hello", "world", "foo"));
+
+    parse(R"("hello",world,"foo")");
+    EXPECT_TRUE(vec_eq("hello", "world", "foo"));
+}
+
+// Test fields with embedded quotes
+TEST_F(ParserTest, EmbeddedQuotes)
+{
+
+    parse(R"("hello ""world""")");
+    EXPECT_TRUE(vec_eq(R"(hello "world")"));
+
+    parse(R"("""hello"" world")");
+    EXPECT_TRUE(vec_eq(R"("hello" world)"));
+
+    parse(R"("""hello world""")");
+    EXPECT_TRUE(vec_eq(R"("hello world")"));
+
+	// (Embedded) quotes outside double-quoted fields are not permitted
+    EXPECT_ANY_THROW(parse(R"(hello "world")"));
+
+    EXPECT_ANY_THROW(parse(R"("hello" world)"));
+
+    EXPECT_ANY_THROW(parse(R"(hello ""world"")"));
+
+    EXPECT_ANY_THROW(parse(R"(""hello"" world)"));
+	
+    EXPECT_ANY_THROW(parse(R"(""hello world"")"));
+
+    EXPECT_ANY_THROW(parse(R"(hello """world""")"));
+
+    EXPECT_ANY_THROW(parse(R"("""hello""" world)"));
+}
+
+// Test fields with embedded delimiters
+TEST_F(ParserTest, EmbeddedDelimiters)
+{
+    parse(R"(",hello")");
+    EXPECT_TRUE(vec_eq(",hello"));
+
+    parse(R"("hello,")");
+    EXPECT_TRUE(vec_eq("hello,"));
+
+    parse(R"("hello, world")");
+    EXPECT_TRUE(vec_eq("hello, world"));
+}
+
+// Test empty fields
+TEST_F(ParserTest, EmptyFields)
+{
+    parse(R"("")");
+    EXPECT_TRUE(vec_eq(""));
+
+    parse(R"("","")");
+    EXPECT_TRUE(vec_eq("", ""));
+
+    parse(R"("","","")");
+    EXPECT_TRUE(vec_eq("", "", ""));
+
+	// Permit empty fields without quotes
+    parse("");
+    EXPECT_TRUE(vec_eq(""));
+
+    parse(",");
+    EXPECT_TRUE(vec_eq("", ""));
+
+    parse(",,");
+    EXPECT_TRUE(vec_eq("", "", ""));
+
+    parse(",hello");
+    EXPECT_TRUE(vec_eq("", "hello"));
+
+    parse("hello,");
+    EXPECT_TRUE(vec_eq("hello", ""));
+}
+
+// Test fields that have only double quotes
+TEST_F(ParserTest, FieldsWithOnlyDoubleQuotes)
+{
+	// ", ", ", "
+    parse(R"("""")");
+    EXPECT_TRUE(vec_eq(R"(")"));
+
+    parse(R"("""","""")");
+    EXPECT_TRUE(vec_eq(R"(")", R"(")"));
+
+    parse(R"("""","""","""")");
+    EXPECT_TRUE(vec_eq(R"(")", R"(")", R"(")"));
+
+    parse(R"("""""")");
+    EXPECT_TRUE(vec_eq(R"("")"));
+
+    parse(R"("""""","""""")");
+    EXPECT_TRUE(vec_eq(R"("")", R"("")"));
+
+    parse(R"("""""","""""","""""")");
+    EXPECT_TRUE(vec_eq(R"("")", R"("")", R"("")"));
+}
+
+// Test fields with multiple lines with \n characters
+TEST_F(ParserTest, MultilineFields)
+{	
+    parse("\"hello\nworld\"");
+    EXPECT_TRUE(vec_eq("hello\nworld"));
+
+	// Newlines outside double-quoted fields are not permitted
+    // because it's interpreted as a line change and the sfcsv::parse
+    // function only parses single lines
+    EXPECT_ANY_THROW(parse("hello\nworld"));
+
+    EXPECT_ANY_THROW(parse("hello world,hello\nworld"));
+}
+
+// Test odd number of quotes inside quoted fields
+TEST_F(ParserTest, OddQuotesInsideField)
+{
+    EXPECT_ANY_THROW(parse(R"(""Hello" odd quotes")"));
+
+    EXPECT_ANY_THROW(parse(R"("Hello "odd" quotes")"));
+
+    EXPECT_ANY_THROW(parse(R"("Hello odd "quotes"")"));
+
+    EXPECT_ANY_THROW(parse(R"(""""Hello""" odd quotes")"));
+
+    EXPECT_ANY_THROW(parse(R"("Hello """odd""" quotes")"));
+
+    EXPECT_ANY_THROW(parse(R"("Hello odd """quotes"""")"));
+}
+
+int main(int argc, char **argv)
+{    
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
