9bf377865083362615a6dc1eefbc0acf8e186e4d
labyrinthofdreams <noemail@localhost.localhost>
2014-06-20 01:37:03 +0300

    Add missing license name

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,5 +1,7 @@
 /****************************************************************************
 
+The MIT License (MIT)
+
 Copyright (c) 2014 https://github.com/labyrinthofdreams
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
